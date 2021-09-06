32d64ea8ea37c1cc90c0c0a37cdfa317d1fb73c4
Milindi-Kodikara <milindikodikara@gmail.com>
2021-09-06 12:28:47 +1000

    Add cron for announcements and core meetings

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Pseudo-Mili
-An announcement bot to make life easier for the real Mili :>
+# Pseudo Mili the bot
+A fiery announcement bot to make life easier for the real Mili :>
+
+![](image.png)
+
+P.S When Aster feels particularly confident, they go by the name Pseudo Mili 💪🏾
+
+Image by the fabulously talented Isaac Chuah Yi Jie.
+Concept design by the ever creative Chaamudi Kodikara.
+
+
+### Set up
+
+1. Follow the instructions on [discord](https://discord.js.org/#/) to add features to your bot
+2. Follow instructions on the AWS console guide in the connect section of your EC2 instance
+3. Run the following commands in EC2
+
+    ```
+   // install git
+   // install npm 
+    git clone <repo name>
+    cd <git repo name>
+    npm i
+    npm install -g forever
+
+    forever start index.js
+    forever list
+    forever stop id
+   
+    // Restart
+    git pull
+    forever start index.js
+   ```
---
+++ index.js
@@ -0,0 +1,102 @@
+// Require the necessary discord.js classes
+const {Client, Intents} = require('discord.js');
+const cron = require('cron');
+const {guildId, token} = require('./config.json');
+const Discord = require('discord.js');
+
+// Create a new client instance
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, 'GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILD_MESSAGES',
+        'DIRECT_MESSAGES', Intents.FLAGS.GUILD_MESSAGES]
+});
+
+var role = ''
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Send an automated announcement on Tuesday at 11 AM
+let tuesday = new cron.CronJob('0 11 * * 2', () => {
+    var guild = client.guilds.cache.get(guildId);
+    role = guild.roles.cache.find(r => r.name === "@everyone");
+
+    const tuesdayEmbed = new Discord.MessageEmbed()
+    .setTitle("Hallo, don't forget to **RSVP** to our *stupefying* event this Friday :dna:")
+    .setDescription(`\nhttps://gdsc.community.dev/rmit-university-melbourne/`)
+    .setColor("#01ACFF");
+
+    client.channels.cache.get('864067701470396426').send({embeds: [tuesdayEmbed], content: role.name});
+})
+
+// Send an automated announcement on Thursday at 11 AM
+let thursday = new cron.CronJob('0 11 * * 4', () => {
+    var guild = client.guilds.cache.get(guildId);
+    role = guild.roles.cache.find(r => r.name === "@everyone");
+
+    const thursdayEmbed = new Discord.MessageEmbed()
+    .setTitle("*Salve, fabulous people :rainbow: :rainbow: :rainbow:*")
+    .setDescription(`\n Just popping here to remind all of you to make sure to **RSVP** to our event **TOMORROW** https://gdsc.community.dev/rmit-university-melbourne/`)
+    .setColor("#01ACFF");
+
+    client.channels.cache.get('864067701470396426').send({embeds: [thursdayEmbed], content: role.name});
+})
+
+// Send an automated announcement on Friday at 5:30 PM
+let fridayEve = new cron.CronJob('30 17 * * 5', () => {
+    var guild = client.guilds.cache.get(guildId);
+    role = guild.roles.cache.find(r => r.name === "@everyone");
+
+    const fridayEveEmbed = new Discord.MessageEmbed()
+    .setTitle("**1 HOUR TO GO ** :bulb:")
+    .setDescription(`\nGetcho butt over here: https://gdsc.community.dev/rmit-university-melbourne/`)
+    .setColor("#01ACFF");
+
+    client.channels.cache.get('864067701470396426').send({embeds: [fridayEveEmbed], content: role.name});
+})
+
+// Send an automated announcement on Friday at 6:30 PM
+let fridayFinal = new cron.CronJob('30 18 * * 5', () => {
+    var guild = client.guilds.cache.get(guildId);
+    role = guild.roles.cache.find(r => r.name === "@everyone");
+
+    const fridayFinalEmbed = new Discord.MessageEmbed()
+    .setTitle("*PSSSTTTT!!!*  **THE TIME HAS COME** :dancer: :fire: ")
+    .setDescription(`\nJoin through here: https://gdsc.community.dev/rmit-university-melbourne/`)
+    .setColor("#01ACFF");
+
+    client.channels.cache.get('864067701470396426').send({embeds: [fridayFinalEmbed], content: role.name});
+})
+
+// Monday meeting
+let mondayMeeting = new cron.CronJob('30 15 * * 1', () => {
+    var guild = client.guilds.cache.get(guildId);
+    core = guild.roles.cache.find(r => r.name === "Core");
+
+    client.channels.cache.get('864067701470396426').send(`${core}, meeting today at 3:30 PM!!!!! If can't make it, well, tell the real Mili!`);
+})
+
+
+// Friday meeting
+let fridayMeeting = new cron.CronJob('00 15 * * 5', () => {
+    var guild = client.guilds.cache.get(guildId);
+    core = guild.roles.cache.find(r => r.name === "Core");
+
+    client.channels.cache.get('864067701470396426').send(`${core}, meeting today at 3:00 PM!!!!! If can't make it, well, tell the real Mili!`);
+})
+
+
+// Send an automated announcement on Thursday at 11 AM
+
+tuesday.start()
+thursday.start()
+fridayEve.start()
+fridayFinal.start()
+
+mondayMeeting.start()
+fridayMeeting.start()
+
+
+// Login to Discord with your client's token
+client.login(token);
